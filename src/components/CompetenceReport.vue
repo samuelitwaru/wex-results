141ce3bcca2ace9e4574518e2a1a77ddb7db3634
samuelitwaru <samuelitwaru@gmail.com>
2022-08-23 12:37:21 +0300

    rectified interface competence report

--- src/components/CompetenceReport.vue
+++ src/components/CompetenceReport.vue
@@ -19,69 +19,35 @@
         </th>
       </thead>
       <tbody>
-        <template
+        <tr
           v-for="subjectReport in subjectReports"
           :key="subjectReport.subject.id"
         >
-          <tr>
-            <td
-              :class="{ 'mini-col': !cv.code }"
-              :rowspan="subjectReport.activities.length || 1"
-            >
-              {{ subjectReport.subject.code }}
-            </td>
-            <td
-              :class="{ 'mini-col': !cv.subject }"
-              :rowspan="subjectReport.activities.length || 1"
-            >
-              {{ subjectReport.subject.name }}
-            </td>
-            <td :class="{ 'mini-col': !cv.totalScores }">
-              {{ subjectReport.activity_total_scores }}
-            </td>
-            <td :class="{ 'mini-col': !cv.average }">
-              {{ subjectReport.activity_average_score }}
-            </td>
-            <td :class="{ 'mini-col': !cv.identifier }">
-              {{ subjectReport.activity_score }}
-            </td>
-            <td :class="{ 'mini-col': !cv.achievement }">
-              {{ subjectReport.activity_score_identifier }}
-            </td>
-            <td
-              :rowspan="subjectReport.activities.length || 1"
-              :class="{ 'mini-col': !cv.initials }"
-            >
-              {{ subjectReport.subject_teacher_initials }}
-            </td>
-          </tr>
-          <tr
-            v-for="(activity, index) in subjectReport.activities.slice(1)"
-            :key="activity.activity.id"
-          >
-            <td
-              :class="{ 'mini-col': !cv.competency }"
-              style="
-                border-left: 1px solid rgba(0, 0, 0, 0.12);
-                padding-left: 0.45rem;
-              "
-            >
-              {{ index + 2 }}. {{ activity?.activity?.name }}
-            </td>
-            <td :class="{ 'mini-col': !cv.mark }">
-              {{ activity?.mark }}
-            </td>
-            <td :class="{ 'mini-col': !cv.score }">
-              {{ activity?.score }}
-            </td>
-            <td :class="{ 'mini-col': !cv.descriptor }">
-              {{ activity?.descriptor }}
-            </td>
-          </tr>
-        </template>
+          <td :class="{ 'mini-col': !cv.code }">
+            {{ subjectReport.subject.code }}
+          </td>
+          <td :class="{ 'mini-col': !cv.subject }">
+            {{ subjectReport.subject.name }}
+          </td>
+          <td :class="{ 'mini-col': !cv.totalScores }">
+            {{ subjectReport.activity_total_scores }}
+          </td>
+          <td :class="{ 'mini-col': !cv.average }">
+            {{ subjectReport.activity_average_score }}
+          </td>
+          <td :class="{ 'mini-col': !cv.identifier }">
+            {{ subjectReport.activity_score }}
+          </td>
+          <td :class="{ 'mini-col': !cv.achievement }">
+            {{ subjectReport.activity_score_identifier }}
+          </td>
+          <td :class="{ 'mini-col': !cv.initials }">
+            {{ subjectReport.subject_teacher_initials }}
+          </td>
+        </tr>
         <tr
           rowspan="8"
-          style="height: 5rem; background-color: rgba(0, 0, 0, 0.02"
+          style="height: 5rem; background-color: rgba(0, 0, 0, 0.02)"
         ></tr>
       </tbody>
     </q-markup-table>
